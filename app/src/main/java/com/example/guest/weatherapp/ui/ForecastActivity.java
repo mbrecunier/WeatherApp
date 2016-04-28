3db3eb5ace7227ee62c6436d4f8b67152dc9ffe1
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-28 10:00:42 -0700

    refactor activities into subpackages

---
+++ app/src/main/java/com/example/guest/weatherapp/ui/ForecastActivity.java
@@ -0,0 +1,7 @@
+package com.example.guest.weatherapp.ui;
+
+/**
+ * Created by Guest on 4/28/16.
+ */
+public class ForecastActivity {
+}
--- app/src/main/java/com/example/guest/weatherapp/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/weatherapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.guest.weatherapp;
+package com.example.guest.weatherapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.guest.weatherapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
