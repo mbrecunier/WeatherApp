1b3d029dab20b9ef171d94b464e1e2647d4bc1df
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-28 11:15:03 -0700

    build credentials for open weather api call

---
+++ app/src/main/java/com/example/guest/weatherapp/Constants.java
@@ -0,0 +1,8 @@
+package com.example.guest.weatherapp;
+
+/**
+ * Created by Guest on 4/28/16.
+ */
+public class Constants {
+    public static final String OPEN_WEATHER_KEY = BuildConfig.OPEN_WEATHER_KEY;
+}
