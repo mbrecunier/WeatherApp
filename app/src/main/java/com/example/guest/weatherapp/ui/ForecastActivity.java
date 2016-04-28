f2b97a195ebd92718ac42b6c0e6ac7dfcb226b43
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-28 13:41:40 -0700

    run getForecast function on creation of ForecastActivity

--- app/src/main/java/com/example/guest/weatherapp/ui/ForecastActivity.java
+++ app/src/main/java/com/example/guest/weatherapp/ui/ForecastActivity.java
@@ -3,22 +3,65 @@ package com.example.guest.weatherapp.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.example.guest.weatherapp.R;
+import com.example.guest.weatherapp.adapters.ForecastListAdapter;
+import com.example.guest.weatherapp.models.Weather;
+import com.example.guest.weatherapp.services.OpenWeatherService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class ForecastActivity extends AppCompatActivity {
     public static final String TAG = ForecastActivity.class.getSimpleName();
-
+    public ArrayList<Weather> mForecast = new ArrayList<>();
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private ForecastListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        Log.i(TAG, location);
+        getForecast(location);
+    }
+
+    private void getForecast(String location) {
+        final OpenWeatherService openWeatherService = new OpenWeatherService();
+
+        openWeatherService.getForecast(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(Call call, Response response) {
+                mForecast = openWeatherService.processResults(response);
+
+                ForecastActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new ForecastListAdapter(getApplicationContext(), mForecast);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ForecastActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+        });
     }
 
 }
