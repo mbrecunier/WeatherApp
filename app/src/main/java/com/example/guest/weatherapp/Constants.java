7337dabca70e938bf2771b05016f3a79d1fc9e08
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-28 14:04:04 -0700

    correct https to http to make correct api call

--- app/src/main/java/com/example/guest/weatherapp/Constants.java
+++ app/src/main/java/com/example/guest/weatherapp/Constants.java
@@ -6,7 +6,7 @@ package com.example.guest.weatherapp;
 public class Constants {
     public static final String OPEN_WEATHER_KEY = BuildConfig.OPEN_WEATHER_KEY;
 
-    public static final String OPEN_WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/forecast/daily?&units=imperial&cnt=7";
+    public static final String OPEN_WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?&units=imperial&cnt=7";
     public static final String OPEN_WEATHER_QUERY_PARAMETER = "q";
     public static final String KEY_QUERY_PARAMETER = "appid";
 }
